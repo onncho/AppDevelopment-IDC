89f2c10192babb6863dffdc04daf7608560b0c7f
onncho <onncho@Onns-MacBook-Pro.local>
2016-06-13 02:54:09 +0300

    ex03

---
+++ Ex03/app.js
@@ -0,0 +1,85 @@
+/**
+ * Created by onncho on 6/11/16.
+ */
+'use strict';
+var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var calcResult = 0;
+
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'www')));
+console.log(path.join(__dirname, 'www'));
+
+
+function normalizePort(val) {
+    var port = parseInt(val, 10);
+
+    if (isNaN(port)) {
+        // named pipe
+        return val;
+    }
+
+    if (port >= 0) {
+        // port number
+        return port;
+    }
+
+    return false;
+}
+
+var port = normalizePort(process.env.PORT || '3000');
+app.set('port', port);
+
+
+/**
+ * Login API
+ */
+app.post("/login", function(req, resp) {
+    var username = req.body.username;
+    var password = req.body.password;
+    username = username.toLowerCase();
+    password = password.toLowerCase();
+    if(username == "admin" && password == "admin"){
+        resp.send("true");
+    } else if (username == "300502465" && password == "onn") {
+        resp.send("true");
+    }
+    else{
+        resp.send("false");
+    }
+});
+
+/**
+ * Save Result API + Get Result API + Funny Quota
+ */
+app.post("/saveResult", function(req,resp){
+    calcResult = req.body.lastResult;
+});
+
+app.get("/getResult",function(req,resp){
+    //resp.sendStatus(200);
+    resp.send(calcResult.toString());
+});
+
+app.get("/randomQuote",function(req,resp){
+    var line1 = "The Girl Has No Name...";
+    var line2 = "Shame... Ding Ding Shame... Ding Ding";
+    var line3 = "The Winter is Coming...";
+    var array = [line1, line2, line3];
+    var pick = Math.floor(Math.random() * array.length);
+    resp.send(array[pick].toString());
+});
+
+/**
+ * Listen on provided port, on all network interfaces.
+ */
+app.listen(port);
+console.log("Server is Listening on : " + port);
+//server.on('error', onError);
+//server.on('listening', onListening);
+
+//module.exports = app;
---
+++ Ex03/www/javascripts/script.js
@@ -0,0 +1,108 @@
+function openCalc() {
+    document.getElementById('readme').style.display = "none";
+    document.getElementById('profileSite').style.display = "none";
+    document.getElementById('calc').style.display = "block";
+
+    try {
+        $.get("/getResult", function (res) {
+            document.getElementById('calcResult').value = res;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+function openWrapper() {
+    document.getElementById('wrapper').style.display = "block";
+    document.getElementById('logindiv').style.display = "none";
+    document.body.style.paddingTop = 0;
+    openProfile();
+}
+
+function openProfile() {
+    document.getElementById('readme').style.display = "none";
+    document.getElementById('profileSite').style.display = "block";
+    document.getElementById('calc').style.display = "none";
+
+    try {
+        $.get("/randomQuote", function (res) {
+            document.getElementById('randomQuote').innerHTML = res;
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function openReadMe() {
+    document.getElementById('readme').style.display = "block";
+    document.getElementById('profileSite').style.display = "none";
+    document.getElementById('calc').style.display = "none";
+}
+
+function evalu() {
+    document.getElementById('calcResult').style.textAlign = "center";
+    document.getElementById('calcResult').style.color = "#FFAE00";
+
+    var result = eval(document.getElementById('calcResult').value);
+    document.getElementById('calcResult').value = result;
+    document.getElementById('calcResult').disabled = true;
+    //save the result on the server
+    try {
+        $.post("/saveResult", {lastResult: result}, function (res) {
+          console.log("saving is done");
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    document.getElementById('startoverbtn').style.display = "block";
+}
+
+function newCalc() {
+    var box = document.getElementById('calcResult');
+    box.style.textAlign = "left";
+    box.style.color = "black";
+    box.disabled = false;
+    box.value = "";
+    document.getElementById('startoverbtn').style.display = "none";
+}
+
+function sendLoginReq() {
+    var userName = document.getElementById("userNameBox").value;
+    var pass = document.getElementById("passBox").value;
+    //TODO: Debug
+    console.log(userName);
+    console.log(pass);
+    $.post("/login", {username: userName, password: pass}, function (res) {
+        if (res == "true") {
+            openWrapper();
+        }
+        else {
+            alert("bad username or password dude!, please try again!");
+        }
+    })
+}
+
+/*
+ function drawButtons() {
+
+ if (document.getElementById('calc').style.display == "") {
+ for (var index = 0; index < 10; index++) {
+ createButtons(index);
+ }
+ }
+ }
+ */
+
+/*
+ function createButtons(num) {
+ var node = document.createElement("input");
+ node.name = num;
+ node.type = "button";
+ node.value = num;
+ node.className = "btn";
+ document.getElementById('calcform').appendChild(node);
+ }
+ */
+
+
